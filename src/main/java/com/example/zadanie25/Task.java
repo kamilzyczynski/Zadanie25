787673eeaf9c6a1cc253fa709fa849d33104d100
kamilzyczynski <kamil.zyczynski@gmail.com>
2023-08-06 18:05:34 +0200

    Rozwiązanie

--- src/main/java/com/example/zadanie25/Task.java
+++ src/main/java/com/example/zadanie25/Task.java
@@ -1,9 +1,9 @@
 package com.example.zadanie25;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 public class Task {
@@ -12,6 +12,28 @@ public class Task {
     private Long id;
     private String name;
     private boolean taskDone;
+
+    private int duration;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private LocalDate completionDate;
+
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Task() {
+        taskDone = false;
+        duration = 0;
+        completionDate = null;
+    }
 
     public String getName() {
         return name;
@@ -27,5 +49,29 @@ public class Task {
 
     public void setTaskDone(boolean taskDone) {
         this.taskDone = taskDone;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public LocalDate getCompletionDate() {
+        return completionDate;
+    }
+
+    public void setCompletionDate(LocalDate completionDate) {
+        this.completionDate = completionDate;
     }
 }
--- src/main/java/com/example/zadanie25/TaskController.java
+++ src/main/java/com/example/zadanie25/TaskController.java
@@ -1,6 +1,14 @@
 package com.example.zadanie25;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class TaskController {
@@ -10,5 +18,76 @@ public class TaskController {
         this.taskRepository = taskRepository;
     }
 
+    @GetMapping("/")
+    public String main(Model model) {
 
+        List<Task> tasksHigh = taskRepository.findAllByPriorityIs(Priority.HIGH);
+        List<Task> tasksMedium = taskRepository.findAllByPriorityIs(Priority.MEDIUM);
+        List<Task> tasksLow = taskRepository.findAllByPriorityIs(Priority.LOW);
+        List<Task> tasksDone = taskRepository.findAllByTaskDoneIsTrue();
+        model.addAttribute("tasksHigh", tasksHigh);
+        model.addAttribute("tasksMedium", tasksMedium);
+        model.addAttribute("tasksLow", tasksLow);
+        model.addAttribute("tasksDone", tasksDone);
+        return "home";
+    }
+
+    @GetMapping("/add")
+    public String add(Model model) {
+
+        model.addAttribute("task", new Task());
+        return "addTask";
+    }
+
+    @GetMapping("/task/edit/{id}")
+    public String edit(@PathVariable Long id, Model model) {
+        Optional<Task> taskById = taskRepository.findById(id);
+
+        if (taskById.isPresent()) {
+            Task task = taskById.get();
+            model.addAttribute("task", task);
+            return "editTask";
+        }
+        return "redirect:/";
+    }
+
+    @PostMapping("/task/edit")
+    public String editTask(Task task) {
+
+        taskRepository.save(task);
+        return "redirect:/";
+    }
+
+    @PostMapping("/add")
+    public String addTask(Task task) {
+
+        taskRepository.save(task);
+        return "redirect:/";
+    }
+
+    @PostMapping("/task/info")
+    public String showtaskInfoForm(@RequestParam("taskId") Long taskId, Model model) {
+        Optional<Task> taskById = taskRepository.findById(taskId);
+
+        if (taskById.isPresent()) {
+            Task task = taskById.get();
+            model.addAttribute("task", task);
+            return "taskInfoForm";
+        }
+
+        return "redirect:/";
+    }
+
+    @PostMapping("/task/save")
+    public String saveTask(@RequestParam("taskId") Long taskId) {
+        Optional<Task> taskById = taskRepository.findById(taskId);
+
+        if (taskById.isPresent()) {
+            Task task = taskById.get();
+            task.setTaskDone(true);
+            task.setPriority(null);
+            taskRepository.save(task);
+        }
+        return "redirect:/";
+    }
 }
